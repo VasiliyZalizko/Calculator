f283aa70d8a124c567a40bc7b77f4f9d853a9996
Vasiliy Zalizko <vasiliy.zalizko@gmail.com>
2023-11-11 20:15:45 +0300

    first commit

---
+++ Problem-1-Calculator/src/IntsCalculator.java
@@ -0,0 +1,18 @@
+public class IntsCalculator implements Ints {
+    protected final Calculator target;
+    public IntsCalculator() {
+        this.target = new Calculator();
+    }
+    @Override
+    public int sum(int arg0, int arg1) {
+        return arg0 + arg1;
+    }
+    @Override
+    public int mult(int arg0, int arg1) {
+        return arg0 * arg1;
+    }
+    @Override
+    public int pow(int a, int b) {
+        return (int) Math.pow(a, b);
+    }
+}
